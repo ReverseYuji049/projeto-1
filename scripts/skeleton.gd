d416fbb2ce7bb9a228c4a2581c867bec75e304db
Yuji <yuji.toda2000@gmail.com>
2026-07-07 14:06:45 -0300

    feat(skeleton): adiciona colisão ao skeleton
    Implementa colisão com RayCast2D ao Skeleton.

--- scripts/skeleton.gd
+++ scripts/skeleton.gd
@@ -11,11 +11,20 @@ enum SkeletonState {
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 # Define uma variável atribuída a HitBox
 @onready var hit_box: Area2D = $HitBox
+# Define uma variável atribuída a RayCast2D (Wall Detector)
+@onready var wall_detector: RayCast2D = $WallDetector
+# Define uma variável atribuída a RayCast2D (Ground Detector)
+@onready var ground_detector: RayCast2D = $GroundDetector
 
-const SPEED = 300.0
+# Variável constante da velocidade
+const SPEED = 30.0
+
 const JUMP_VELOCITY = -400.0
 
 var status: SkeletonState # Recebe os valores da Enum
+
+# Define a direção do Skeleton
+var direction = 1
 
 # Começa no estado de walk
 func _ready() -> void:
@@ -40,15 +49,24 @@ func go_to_walk_state():
 	animated_sprite_2d.play("walk") # Animação de andar
 
 func go_to_dead_state():
-	status = SkeletonState.dead # Define o status como dead
-	animated_sprite_2d.play("dead") # Animação de morto
-	hit_box.process_mode = Node.PROCESS_MODE_DISABLED # Desativa a HitBox
+	status = SkeletonState.dead; # Define o status como dead
+	animated_sprite_2d.play("dead"); # Animação de morto
+	hit_box.process_mode = Node.PROCESS_MODE_DISABLED; # Desativa a HitBox
+	velocity = Vector2.ZERO; # Zera a velocidade ao ser derrotado
 	
 func walk_state(_delta):
-	pass
+	velocity.x = SPEED * direction;
+	# Localiza colizão
+	if wall_detector.is_colliding():
+		scale.x *= -1; # Inverte a escala
+		direction *= -1; # Inverte a direção
+	# Localiza fim de terreno
+	if not ground_detector.is_colliding():
+		scale.x *= -1; # Inverte a escala
+		direction *= -1; # Inverte a direção
 
 func dead_state(_delta):
-	pass
+	pass;
 
 # Ao tomar dano, chama a função go_to_dead_state
 func take_damage():
